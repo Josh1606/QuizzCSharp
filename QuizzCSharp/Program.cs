6ac2064a7ab54773dfe5a0ca5c35b9fdef51f5c9
Cedricnwahba77 <Nwahbac@gmail.com>
2021-01-22 15:50:54 +0100

    cedric

--- QuizzCSharp/Program.cs
+++ QuizzCSharp/Program.cs
@@ -1,46 +1,82 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.Threading;
+
 namespace Projet1_app_console_cédric_
 {
     class Program
     {
         static void Main(string[] args)
         {
+            static void wrongAnswer()
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.WriteLine("Mauvaise réponse...");
+            }
+            static void goodAnswer()
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Thread.Sleep(100);
+                Console.WriteLine("Bonne réponse !!!");
+            }
+
             string answer = "";
             int score = 0;
-            Console.WriteLine("Quelles sont les bonnes boucles en C# ?");
-            Console.WriteLine("");
-            Console.WriteLine("Reponse A: do...while; while; else if; etc...");
-            Console.WriteLine("Reponse B: if...else; switch; else if; etc...");
-            Console.WriteLine("Reponse C: Console.Write(); Console.ReadLine(); Console.ReadKey(); etc...");
-            Console.WriteLine("Reponse D: //; /*; */; etc...");
-            Console.WriteLine("");
+            Console.WriteLine("Quelles sont les bonnes boucles en C# ? \r\n\r\n" +
+                "Reponse A: do...while; while; else if; etc...\r\n" +
+                "Reponse B: if...else; switch; else if; etc...\r\n" +
+                "Reponse C: Console.Write(); Console.ReadLine(); Console.ReadKey(); etc... \r\n" +
+                "Reponse D: //; /*; */; etc...");
             answer = Console.ReadLine();
-            if(answer == "A" || answer == "a" || answer == "Reponse A" || answer == "reponse a")
+            if (answer == "A" || answer == "a" || answer == "Reponse A" || answer == "reponse a")
             {
                 score++;
-                Console.WriteLine("Bonne reponse !");
+                goodAnswer();
             }
             else
             {
-                Console.WriteLine("Dommage la reponse était la réponse A");
+                wrongAnswer();
             }
-            Console.WriteLine("Comment déclarer une enumeration sur C# ?");
-            Console.WriteLine("");
-            Console.WriteLine("Reponse A: Console.ReadLine();");
-            Console.WriteLine("Reponse B: Enumeration");
-            Console.WriteLine("Reponse C: enum");
-            Console.WriteLine("Reponse D: Console.EnumLine()");
+            Thread.Sleep(3000);
+            Console.Clear();
+            Console.WriteLine("Comment déclarer une enumeration sur C# ? \r\n\r\n" +
+            "Reponse A: Console.ReadLine();\r\n" +
+            "Reponse B: Enumeration\r\n" +
+            "Reponse C: enum\r\n" +
+            "Reponse D: Console.EnumLine()");
             answer = Console.ReadLine();
-            if(answer == "C" || answer == "c" || answer == "Reponse C" || answer == "reponse c")
+            if (answer == "C" || answer == "c" || answer == "Reponse C" || answer == "reponse c")
             {
                 score++;
-                Console.WriteLine("Bonne reponse !");
+                goodAnswer();
             }
             else
             {
-                Console.WriteLine("Dommage la reponse était la reponse C");
+                wrongAnswer();
             }
+            Console.WriteLine("Votre score est : " + score);
+            Thread.Sleep(3000);
+            Console.Clear();
             Console.ReadKey();
         }
     }
